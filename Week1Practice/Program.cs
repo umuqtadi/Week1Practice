408b0289f7350ebc0e742ca45a7f5902e920b982
Dustin Kraft <kraftdr@gmail.com>
2014-06-17 11:38:51 -0600

    Updates

--- Week1Practice/Program.cs
+++ Week1Practice/Program.cs
@@ -13,29 +13,37 @@ namespace Week1Practice
             // DECLARING VARIABLES
             // 1. Declare a string variable called "myName" and set it equal to your name
             // 2. Declare a number varible called "myAge" and set it equal to your age
-            // 3. Delcare a boolean value called "myBool" and set it equal to true or false;
-            // 4. Declare an array of strings called "products" and set it equal to "basketball", "baseball glove", "tennis shoes", "hockey puck"
+            // 3. Declare a boolean value called "myBool" and set it equal to true or false;
+            // 4. Declare a new list of strings called "productsList" and set it equal to "basketball", "baseball glove", "tennis shoes", "hockey puck"
 
             // PRINTING VARIABLES TO THE CONSOLE USING CONSOLE.WRITELINE()
             // 1. Print your name to the console using the format: "My name is <myName> and I'm a beast of a programmer"
             // 2. Print out your age to the console using the format: "I am <myAge> years awesome."
             // 3. Print out your boolean value using the format: "I set my boolean value equal to <myBool>"
-            // 4. Using a For loop, print out each value in your products array.
+            // 4. Using a For loop, print out each value in productsList.
 
             // FOR LOOP PRACTICE
             // 1. Create a for loop that prints out the numbers 1 to 10.
             // 2. Create a for loop that prints out the numbers 10 to 1.
             // 3. Create a for loop that prints out the numbers 10 to 30, only printing the even numbers
             // 4. Create a for loop that prints out the numbers 100 to 75, only printing every 5th number (100, 95, 90, 85, 80, 75)
-           
+
+            // WHILE LOOP PRACTICE
+            // 1. Create a while loop that prints out the numbers from 1 to 10.
+            // 2. Create a while loop that prints out the numbers from 10 to 1.
+            // 3. Create a while loop that prints out the numbers from 15 to 30, only printing the even numbers
+            // 4. Create a while loop that prints out the numbers from 100 to 75, only printing every 5th number (100, 95, 90, 85, 80, 75)
+            // 5. Create a while loop that prints out the numbers from 1 to 10 until it reaches one that is divisible by 4.
+
             // PUTTING IT TOGETHER
+            // Use either FOR or WHILE loops to accomplish the following:
             // 1. Print out the number of letters in your name using the format: "My name, <myName>, has <number of letters> in it."
-            // 2. Print out the numbers of elements in your array using the format: "My array has <number of elements> in it."
-            // 3. Print out the number of letters in each element of your array using the format: "<product name> has <number of letters> in it."
+            // 2. Print out the number of items in your list using the format: "My product list has <number of items> in it."
+            // 3. Print out the number of letters of each item in productsList using the format: "<product> has <number of letters> in it."
 
             // DECLARING AND CALLING FUNCTIONS
             // 1. Create a function called "Greeting" that takes one string parameter called "name".  This function will print "Hello <name>"
-            // 2. Call the Greeting function by passing in "Geronimo Kackson" as your parameter.
+            // 2. Call the Greeting function by passing in "Geronimo Jackson" as your parameter.
             // 3. Call the Greeting function by passing in myName as your parameter.
              
             // 4. Create a function called "DoubleIt" that takes one integer parameter called "number".  This function will print "<number> doubled is <number * 2>"
@@ -65,6 +73,7 @@ namespace Week1Practice
             //DECLARING AND CALLING RETURN FUNCTIONS
             // 1. Create a function called "NewGreeting" that takes one string parameter called "name".  This function will return a string in the format of "Hello, <name>"
             // 2. Using your NewGreeting function as the parameter for Console.WriteLine(), call your NewGreeting function using the parameter "Neil deGrasse-Tyson"
+            //          EX: Console.WriteLine(FunctionCall("myParameter"));
             // 3. Using your NewGreeting function as the parameter for Console.WriteLine(), call your NewGreeting function using the parameter myName.
             
             // 4. Create a function called "TripleIt" that takes one integer parameter called "number".  This function will return the number times 3.
@@ -92,5 +101,15 @@ namespace Week1Practice
 
 
         }
+
+        //START -- FUNCTION DECLARATIONS 
+
+        //Example function declaration
+        static void MyFunction(string myParameter)
+        {
+            //Code block
+        }
+
+        //END   -- FUNCTION DECLARATIONS
     }
 }
