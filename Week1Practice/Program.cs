85ed0c1ee26c6e40050dc187dae3896937ffd2f1
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-07 22:21:56 -0600

    Partially Complete

--- Week1Practice/Program.cs
+++ Week1Practice/Program.cs
@@ -12,22 +12,49 @@ namespace Week1Practice
         {
             // DECLARING VARIABLES
             // 1. Declare a string variable called "myName" and set it equal to your name
+            string myName = "Umar Muqtadir";
             // 2. Declare a number varible called "myAge" and set it equal to your age
+            int myAge = 25;
             // 3. Declare a boolean value called "myBool" and set it equal to true or false;
+            bool myBool = true;
             // 4. Declare a new list of strings called "productsList" and set it equal to "basketball", "baseball glove", "tennis shoes", "hockey puck"
-
+            List<string> productsList = new List<string>();
+            {
+                productsList.Add("basketball");
+                productsList.Add("baseball glove");
+                productsList.Add("tennis shoes");
+                productsList.Add("hockey puck");
+            }
             // PRINTING VARIABLES TO THE CONSOLE USING CONSOLE.WRITELINE()
             // 1. Print your name to the console using the format: "My name is <myName> and I'm a beast of a programmer"
+            Console.WriteLine("My name is " + myName + " I'm a beast of a programmer");
             // 2. Print out your age to the console using the format: "I am <myAge> years awesome."
+            Console.WriteLine("I am " + myAge + " years awesome.");
             // 3. Print out your boolean value using the format: "I set my boolean value equal to <myBool>"
+            Console.WriteLine("I set my boolean value equal to " + myBool);
             // 4. Using a For loop, print out each value in productsList.
-
+           
             // FOR LOOP PRACTICE
             // 1. Create a for loop that prints out the numbers 1 to 10.
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
             // 2. Create a for loop that prints out the numbers 10 to 1.
+            for (int i = 10; i > 0 ; i--)
+            {
+                Console.WriteLine(i);
+            }
             // 3. Create a for loop that prints out the numbers 10 to 30, only printing the even numbers
+            for (int i = 10; i <= 30; i+= 2)
+            {
+                Console.WriteLine(i);
+            }
             // 4. Create a for loop that prints out the numbers 100 to 75, only printing every 5th number (100, 95, 90, 85, 80, 75)
-
+            for (int i = 75; i <= 100; i+=5)
+            {
+                Console.WriteLine(i);
+            }
             // WHILE LOOP PRACTICE
             // 1. Create a while loop that prints out the numbers from 1 to 10.
             // 2. Create a while loop that prints out the numbers from 10 to 1.
@@ -45,7 +72,7 @@ namespace Week1Practice
             // 1. Create a function called "Greeting" that takes one string parameter called "name".  This function will print "Hello <name>"
             // 2. Call the Greeting function by passing in "Geronimo Jackson" as your parameter.
             // 3. Call the Greeting function by passing in myName as your parameter.
-             
+
             // 4. Create a function called "DoubleIt" that takes one integer parameter called "number".  This function will print "<number> doubled is <number * 2>"
             // 5. Call your DoubleIt Function by passing in 1337 as your parameter.
             // 6. Call your DoubleIt function by passing in myAge as your parameter.
@@ -75,7 +102,7 @@ namespace Week1Practice
             // 2. Using your NewGreeting function as the parameter for Console.WriteLine(), call your NewGreeting function using the parameter "Neil deGrasse-Tyson"
             //          EX: Console.WriteLine(FunctionCall("myParameter"));
             // 3. Using your NewGreeting function as the parameter for Console.WriteLine(), call your NewGreeting function using the parameter myName.
-            
+
             // 4. Create a function called "TripleIt" that takes one integer parameter called "number".  This function will return the number times 3.
             // 5. Using your TripleIt function as part of the parameter for Console.WriteLine(), 
             //      call your TripleIt function using the parameter of 10 to print out "10 tripled is <TripleIt Function Call>"
@@ -99,7 +126,7 @@ namespace Week1Practice
             //      endNum = Call TripleIt with the parameter RealMultiply with the parameters: myAge, 7
             //      increment = Call TripleIt with the parameter of myAge minus RealMultiply with the parameters 2, 4.
 
-
+            Console.ReadKey();
         }
 
         //START -- FUNCTION DECLARATIONS 
